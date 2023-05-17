dcb3bccbc39562b72e4f81d80546dfb9db32cd15
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-05-16 21:44:17 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net',
   redirectUri: 'https://brave-pond-01ea24c10.3.azurestaticapps.net',
-  clientID: 'ba171311-b438-4c6a-ba16-28d9facb9e60',  
+  clientID: 'ffd10c5b-0d31-4024-bc9c-4d9681b0b82e',  
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3'
 };
