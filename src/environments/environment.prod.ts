4606ae54b34d5f6ad96f5453d06e2a049222db5d
proyhkh <128107690+proyhkh@users.noreply.github.com>
2023-05-17 15:22:53 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   urlApiBovis: 'bovis-dev-api-up.azurewebsites.net',
   redirectUri: 'https://brave-pond-01ea24c10.3.azurestaticapps.net',
-  clientID: 'ffd10c5b-0d31-4024-bc9c-4d9681b0b82e',  
+  clientID: '02d0add9-98d8-4c97-9bf8-06ab1f88344a',  
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3'
 };
